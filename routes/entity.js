983c4b9de3f4015bf09d7c85c5e51e0a3278430b
聂红亮 <niehl@ucweb.com>
2014-07-06 15:44:32 +0800

    [UPD]

--- routes/entity.js
+++ routes/entity.js
@@ -1,15 +1,47 @@
-exports.getItem = function (req, res, next) {
+var bll = require('../private/bll/entity');
+
+exports.getItem = function (req, res) {
 
 }
 
-exports.getList = function (req, res, next) {
+exports.getList = function (req, res) {
 
 }
 
-exports.publish = function (req, res, next) {
-    if (!req.session || !req.session.user) {
+// 发布商品
+exports.publish = function (req, res) {
+    var user = req.session && req.session.user;
+    if (!user) {
+        res.send({ status: 'forbidden' });
+        return;
+    }
+    
+}
+
+// 竞拍商品
+exports.auction = function (req, res) {
+    var user = req.session && req.session.user;
+    if (!user) {
         res.send({ status: 'forbidden' });
         return;
     }
 
+}
+
+// 评论商品
+exports.comment = function (req, res) {
+    var user = req.session && req.session.user;
+    if (!user) {
+        res.send({ status: 'forbidden' });
+        return;
+    }
+}
+
+// 收藏商品
+exports.favorite = function (req, res) {
+    var user = req.session && req.session.user;
+    if (!user) {
+        res.send({ status: 'forbidden' });
+        return;
+    }
 }
