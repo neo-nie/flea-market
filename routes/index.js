3171fcbd6a66f505c12181bb31fe2311beb70934
wurh5566 <wurh@ucweb.com>
2014-07-08 16:37:10 +0800

    [UPD]报错

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 //首页
-exports.index = function (req, res){
-    res.render('index', { title: '跳蚤街' });
+exports.index = function (req, res) {
+  res.render('index', { title: '爱售网 售（show）出你的二手货！' });
 }
 
 exports.login = function (req, res) {
-    res.render('login', { title: '登录页' });
+  res.render('login', { title: '登录页' });
 }
 
-   res.render('index', { title: '爱售网 售（show）出你的二手货！' 
-});
+exports.entity = function (req, res) {
+  res.render('entity', { title: '商品页' });
+}
 
-exports.entity = function (req, res){
-    res.render('entity', { title: '商品页' });
-}//发布页
-exports.publish = function (req, res){
-    res.render('publish', { title: '发布页' });
+//发布页
+exports.publish = function (req, res) {
+  res.render('publish', { title: '发布页' });
 }
