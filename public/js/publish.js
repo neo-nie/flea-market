fae3d92f21b6bd10a1de50ec56945e92249c35ef
wurh5566 <wurh@ucweb.com>
2014-07-09 14:55:06 +0800

    [UPD]上传样式

--- public/js/publish.js
+++ public/js/publish.js
@@ -1,24 +1,18 @@
 $(document).ready(function () {
-  bindEvent();
   $('#file_upload').uploadifive({
-    'auto': false,
+    'auto': true,
     'buttonClass'  : 'upload-btn',
-    'buttonText': '选择',
+    'buttonText': '选择文件',
     'multi': false,
     'formData': {
       'timestamp': '1404872336',
       'token': 'bca8aeae9d6e2dd0a049987dec252aee'
     },
-    'queueID': 'queue',
     'uploadScript': 'api/upload',
     'onUploadComplete': function (file, data) {
-      console.log(data);
+      var type = file.type.split('/')[1];
+      type = type.replace('jpeg','jpg');
+      $('#imgType').html(type);
     }
   });
 });
-
-function bindEvent() {
-  $('#upload_start').on('click', function () {
-    $('#file_upload').uploadifive('upload');
-  });
-}
