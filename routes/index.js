567570e70b6d77d3286ca10757b44f5b0a59ab14
聂红亮 <niehl@ucweb.com>
2014-07-10 10:11:14 +0800

    [UPD] 调整登录逻辑，减少嵌套层次

--- routes/index.js
+++ routes/index.js
@@ -1,41 +1,48 @@
 var request = require('request');
+var Q = require('q'),
+    get = Q.nbind(request.get, request);
 
 //首页
 exports.index = function (req, res) {
   res.render('index', { title: '爱售网 售（show）出你的二手货！' });
 }
 
+
+var VALIDATE_URL = 'https://ilogin.ucweb.com/sso/serviceValidate',
+    LOGIN_URL = 'https://ilogin.ucweb.com/sso/login',
+    ENTRY_URL = 'http://10.1.88.176:3000/login';
 //登录页
 exports.login = function (req, res) {
     var ticket = req.query.ticket;
-    var user = req.session && req.session.user;
 
-    var VALIDATE_URL = 'https://ilogin.ucweb.com/sso/serviceValidate',
-        LOGIN_URL = 'https://ilogin.ucweb.com/sso/login',
-        ENTRY_URL = 'http://10.1.88.176:3000/login';
-
-    if (ticket) {
-        var url = VALIDATE_URL +'?service=' + ENTRY_URL + '&ticket=' + ticket;
-        console.log(url);
-        request(url, function (err, resp, body){
-            if (err) console.log(err);
-            var regex = /\<cas\:username\>(.*)\<\/cas\:username\>/;
-
-            console.log(body);
-            if (regex.test(body)){  //登录成功
-                console.log('登录成功');
-                req.session.user = RegExp.$1;
-                res.redirect('/');
-            }
-            else {  //登录失败
-                console.log('登录失败');
-                res.redirect(LOGIN_URL + '?service=' + ENTRY_URL);
-            }
-        })
-    }else if (!user) {    //如果没有登录
-        res.redirect(LOGIN_URL + '?service=' + ENTRY_URL);
+    if (req.session && req.session.user) {  //已经登录
+        console.log(req.session.user);
+        res.redirect(req.query.from || '/');
+        return;
     }
-  // res.render('login', { title: '登录页' });
+    if (!ticket) {  //没有令牌，跳转到oa登录页面
+        res.redirect(LOGIN_URL + '?service=' + ENTRY_URL);
+        return;
+    }
+
+    var url = VALIDATE_URL +'?service=' + ENTRY_URL + '&ticket=' + ticket;
+    get(url).then(function (data){
+        var resp = data[0], body = data[1];
+        var regex = /\<cas\:username\>(.*)\<\/cas\:username\>/;
+        console.log(body);
+        
+        if (!regex.test(body)){  //登录失败，跳转到oa登录页
+            throw new Error('登录失败');
+        }
+
+        //登录成功，跳到首页
+        // console.log('登录成功');
+        req.session.user = RegExp.$1;
+        res.redirect('/');
+    }).fail(function (err){
+        console.log(err);
+        res.redirect(LOGIN_URL + '?service=' + ENTRY_URL);
+    })
 }
 
 //商品详情页
@@ -46,4 +53,9 @@ exports.entity = function (req, res) {
 //发布页
 exports.publish = function (req, res) {
   res.render('publish', { title: '发布页' });
+}
+
+//用户页
+exports.user = function (req, res) {
+    res.render('user', { title: '用户页'});
 }
--- test.js
+++ test.js
@@ -1,18 +1,40 @@
-var option = {
-    host     : '10.1.72.154',
-    port     : 3306,
-    user     : 'root',
-    password : 'root',
-    database : 'flea',
-    connectionLimit: 10
-};
-var mysql      = require('mysql'),
-    connection = mysql.createConnection(option);
-var Q = require('q');
+// var option = {
+//     host     : '10.1.72.154',
+//     port     : 3306,
+//     user     : 'root',
+//     password : 'root',
+//     database : 'flea',
+//     connectionLimit: 10
+// };
+// var mysql      = require('mysql'),
+//     connection = mysql.createConnection(option);
+// var Q = require('q');
 
-var beginTransaction = Q.nbind(connection.beginTransaction, connection),
-    query = Q.nbind(connection.query, connection),
-    commit = Q.nbind(connection.commit, connection),
-    rollback = Q.nbind(connection.rollback, connection);
+// var beginTransaction = Q.nbind(connection.beginTransaction, connection),
+//     query = Q.nbind(connection.query, connection),
+//     commit = Q.nbind(connection.commit, connection),
+//     rollback = Q.nbind(connection.rollback, connection);
 
-beginTransaction().then()
+// beginTransaction().then()
+
+var xml2js = require('xml2js');
+
+var xml = "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'> \
+    <cas:authenticationSuccess> \
+        <cas:user>niehl</cas:user> \
+        <cas:attributes> \
+    \
+                    <cas:uid>niehl</cas:uid> \
+\
+                    <cas:username>niehl</cas:username> \
+\
+                    <cas:cnname>聂红亮</cas:cnname> \
+\
+        </cas:attributes> \
+\
+\
+    </cas:authenticationSuccess> \
+</cas:serviceResponse>";
+xml2js.parseString(xml, function (err, result){
+    console.log(result['cas:serviceResponse']['cas:authenticationSuccess']);
+})
