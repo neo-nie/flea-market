6e209ca204fd6905a482dc032de019f7093ee054
聂红亮 <niehl@ucweb.com>
2014-07-12 15:14:58 +0800

    [UPD]

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 //首页
 exports.index = function (req, res) {
-  res.render('index', { title: '爱售网 售（show）出你的二手货！' });
+  res.render('index', { title: '爱售网 售（show）出你的二手货！', user: req.session.user });
 }
 
 //登录页
@@ -10,15 +10,15 @@ exports.login = function (req, res) {
 
 //商品详情页
 exports.entity = function (req, res) {
-  res.render('entity', { title: '商品页' });
+  res.render('entity', { title: '商品页', user: req.session.user });
 }
 
 //发布页
 exports.publish = function (req, res) {
-  res.render('publish', { title: '发布页' });
+  res.render('publish', { title: '发布页', user: req.session.user });
 }
 
 //用户页
 exports.user = function (req, res) {
-  res.render('user', { title: '用户页'});
+  res.render('user', { title: '用户页', user: req.session.user});
 }
