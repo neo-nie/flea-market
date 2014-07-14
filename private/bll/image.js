22e2e142b3c26a9f1582dcfeb30b2f58aec6e69f
wurh5566 <wurh@ucweb.com>
2014-07-14 10:08:19 +0800

    [UPD]修改上传

--- private/bll/image.js
+++ private/bll/image.js
@@ -16,13 +16,28 @@ exports.upload = function (file, userId){
   var sql = 'insert into image(user_id, create_time, url) values(?, ?, ?)';
   var deferred = Q.defer();
 
-  fs.rename(file.path, savepath, function (err) {
+  if (!file) {
+    res.send({ status: 'failed', message: 'no file' });
+    return;
+  }
+
+  var fileName = new Date().getTime().toString()+'.jpg';
+  var savepath = path.join('./public/img/entity',fileName);
+  var is = fs.createReadStream(file.path);
+  var os = fs.createWriteStream(savepath);
+  is.pipe(os);
+  is.on('end',function() {
+   // fs.unlinkSync(file.path);
+    //res.send({ status: 'success', id: '123', url:'/img/entity/'+fileName});
+  });
+
+ /* fs.rename(file.path, savepath, function (err) {
     if (err) {
       deferred.reject(err);
     }
     var url = '/upload/' + uid + '/' + encodeURIComponent(filename);
     res.send({ status: 'success', id: id, url: url });
-  });
+  });*/
 
   return deferred.promise;
 }
--- routes/image.js
+++ routes/image.js
@@ -13,26 +13,10 @@ var imageDir = path.join(__dirname, 'public', 'img', 'entity');
  */
 exports.upload = function (req, res, next) {
   var user = req.session && req.session.user;
-  /*   if (!user) {
-   res.send({ status: 'forbidden' });
-   return;
-   }*/
-
-  var file = req.files && req.files.Filedata;
-  console.log(req.files)
-  if (!file) {
-    res.send({ status: 'failed', message: 'no file' });
+  if (!user) {
+    res.send({ status: 'forbidden' });
     return;
   }
-
-  var fileName = new Date().getTime().toString()+'.jpg';
-  var savepath = path.join('./public/img/entity',fileName);
-  var is = fs.createReadStream(file.path);
-  var os = fs.createWriteStream(savepath);
-  is.pipe(os);
-  is.on('end',function() {
-    fs.unlinkSync(file.path);
-    res.send({ status: 'success', id: '123', url:'/img/entity/'+fileName});
-  });
-
+  var file = req.files && req.files.Filedata;
+  var img = bll.upload(file, user.uid);
 }
