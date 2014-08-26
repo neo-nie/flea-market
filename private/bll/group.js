17611d5de8ff32d9503c033407b0c8d5a3b38771
聂红亮 <niehl@ucweb.com>
2014-08-26 14:33:07 +0800

    [ADD] 增加用户组group.js（交易圈）

---
+++ private/bll/group.js
@@ -0,0 +1,8 @@
+// 用户群组、交易圈属性
+// id、name、alias、create_time、update_time、desc
+// 创建交易圈、发送邀请、验证用户、获取交易圈中的商品
+
+exports.create = function (name, alias, creator) {
+  // 用户创建交易圈，填写交易圈名称、发送邀请邮件，获取邀请链接
+  // 其他用户通过邀请链接加入到交易圈，需通过管理员审核
+}
